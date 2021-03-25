58232788d1d14c58d1594ae2be8cfeb0614f8922
kasy1tu <74841065+kasy1tu@users.noreply.github.com>
2021-03-24 18:41:13 -0700

    created table

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
+const tbody = d3.select("tbody");
+
+function buildTable(data) {
+    tbody.html(""); 
+    data.forEach((dataRow) => {
+        const row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+
+    });
+}
+
 // YOUR CODE HERE!
